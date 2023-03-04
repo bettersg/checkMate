3dc84edc28f96576de4d060707cf18a72ed1f9bd
Bing Wen Tan <bingwen.cpf@gmail.com>
2023-03-04 17:01:35 +0800

    cosine similarity fixes

--- functions/definitions/calculateSimilarity.js
+++ functions/definitions/calculateSimilarity.js
@@ -8,37 +8,38 @@ if (!admin.apps.length) {
 exports.calculateSimilarity = async function (messageToCompare) {
     const db = admin.firestore()
     // strip any url in the message
-    messageToCompare = messageToCompare.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
+    messageToCompare = messageToCompare.replace(/(?:https?|ftp):\/\/[\n\S]/g, '');
     // strip phone numbers of minimum 7 digits as they can vary as well
-    messageToCompare = messageToCompare.replace(/[0-9]{7,}+/g, '')
-    
+    messageToCompare = messageToCompare.replace(/[0-9]{7,}/g, '')
+
     // stores the results of the comparison between each message in db and the current message to evaluate
-    let comparisonScoresTable = [] 
+    let comparisonScoresTable = []
     let currentSimilarityScore = 0
 
     // get all the messages of type text from firestore
     const spamMessages = await db.collection('messages').where('type', '==', 'text').get();
     // iterate over the messages and compare with current then add to the table for further sorting
-    spamMessages.forEach(spamMessageDoc =>{
+    spamMessages.forEach(spamMessageDoc => {
         const spamMessage = spamMessageDoc.data();
-        if(spamMessage.text != "") {
+        if (spamMessage.text != "") {
             // strip urls from current message to allow variation comparison
             let currentMessage = spamMessage.text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
             // strip phone numbers of minimum 7 digits as they can vary as well
-            currentMessage = currentMessage.replace(/[0-9]{7,}+/g, '')
+            currentMessage = currentMessage.replace(/[0-9]{7,}/g, '')
             // if we have a match after stripping we give 100% score as it is a variation, otherwise we compute the cosineSimilarity score
             if (currentMessage == messageToCompare) {
-                comparisonScoresTable.push( {message: spamMessage.text, score: 100} )    
+                comparisonScoresTable.push({ message: spamMessage.text, score: 100 })
             } else {
                 currentSimilarityScore = getSimilarityScore(textCosineSimilarity(spamMessage.text, messageToCompare))
-                comparisonScoresTable.push( {message: currentMessage, score: currentSimilarityScore} )  
+                comparisonScoresTable.push({ message: currentMessage, score: currentSimilarityScore })
             }
         }
     })
 
     // sort by similarity score and return only the highest scoring message
     if (comparisonScoresTable.length > 0) {
-        let sortedResults = comparisonScoresTable.sort((r1, r2) => (r1.score > r2.score) ? 1 : (r1.score < r2.score) ? -1 : 0);
+        let sortedResults = comparisonScoresTable.sort((r1, r2) => (r1.score > r2.score) ? 1 : (r1.score < r2.score) ? -1 : 0); //I suspect this is yielding
+        console.log(sortedResults[sortedResults.length - 1]);
         return sortedResults[0]
     } else {
         return {}
--- functions/definitions/userHandlers.js
+++ functions/definitions/userHandlers.js
@@ -4,7 +4,7 @@ const { Timestamp } = require('firebase-admin/firestore');
 const { sendWhatsappTextMessage, markWhatsappMessageAsRead } = require('./common/sendWhatsappMessage');
 const { mockDb, sleep, getReponsesObj } = require('./common/utils');
 const { downloadWhatsappMedia, getHash } = require('./common/mediaUtils');
-const calculateSimilarity = require('./calculateSimilarity')
+const { calculateSimilarity } = require('./calculateSimilarity')
 const { defineString } = require('firebase-functions/params');
 const runtimeEnvironment = defineString("ENVIRONMENT")
 
@@ -83,12 +83,13 @@ async function newTextInstanceHandler(db, {
   let messageId;
   if (textMatchSnapshot.empty) {
     // 2 - if there is no exact match, then perform a cosine similarity calculation
-    let similarity = calculateSimilarity(text)
+    let similarity = await calculateSimilarity(text)
+    console.log(similarity);
     if (similarity != {}) {
       const bestMatchingDocument = similarity.message
       const similarityScore = similarity.score
-      console.log(bestMatchingDocument)
-      console.log(similarityScore)
+      // console.log(bestMatchingDocument)
+      // console.log(similarityScore)
     }
     let writeResult = await db.collection('messages').add({
       type: "text", //Can be 'audio', 'button', 'document', 'text', 'image', 'interactive', 'order', 'sticker', 'system', 'unknown', 'video'. But as a start only support text and image
