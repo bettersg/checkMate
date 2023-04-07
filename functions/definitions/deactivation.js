0780b5a935a1b37d26e22fa49083b699ef2b493d
Bing Wen Tan <bingwen.cpf@gmail.com>
2023-04-07 21:10:23 +0800

    deactivation function

--- functions/definitions/deactivation.js
+++ functions/definitions/deactivation.js
@@ -1,5 +1,11 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const { sendWhatsappTemplateMessage } = require("./common/sendWhatsappMessage");
+const { Timestamp } = require('firebase-admin/firestore');
+
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
 
 exports.scheduledFunction = functions
   .region("asia-southeast1")
@@ -7,10 +13,26 @@ exports.scheduledFunction = functions
   .timeZone('Asia/Singapore')
   .onRun(async (context) => {
     const db = admin.firestore();
+    const cutoffHours = 72;
     const activeCheckMatesSnap = await db.collection("factCheckers").where("isActive", "==", true).get();
     const promisesArr = activeCheckMatesSnap.docs.map(async (doc) => {
-      const id = doc.id;
-      const voteRequestsQuerySnap = await db.collectionGroup('voteRequests').where('platformId', '==', factCheckerId).get();
-
+      const lastVotedTimestamp = doc.get("lastVotedTimestamp") ?? Timestamp.fromDate(new Date(0));
+      const factCheckerId = doc.id;
+      const lastVotedDate = lastVotedTimestamp.toDate();
+      //set cutoff to 72 hours ago
+      const cutoffDate = new Date(Date.now() - cutoffHours * 60 * 60 * 1000)
+      const cutoffTimestamp = Timestamp.fromDate(cutoffDate);
+      const voteRequestsQuerySnap = await db.collectionGroup('voteRequests')
+        .where('platformId', '==', factCheckerId)
+        .where('createdTimestamp', '<', cutoffTimestamp)
+        .where('category', '==', null)
+        .get();
+      if (!voteRequestsQuerySnap.empty && lastVotedDate < cutoffDate) {
+        console.log(`${factCheckerId}, ${doc.get('name')}`)
+        //await sendWhatsappTemplateMessage("factChecker", factCheckerId, "deactivation_notification", "en", [doc.get("name") || "CheckMate", `${cutoffHours}`], [`${factCheckerId}`])
+        //return doc.ref.update({ isActive: false });
+        return
+      }
     });
+    await Promise.all(promisesArr);
   });
