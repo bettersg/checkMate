62b734c00efe6da87dac00b5acd1aa3001f4a32c
Bing Wen Tan <bingwen.cpf@gmail.com>
2023-05-07 22:36:41 +0800

    removed auxillary function

--- functions/src/index.js
+++ functions/src/index.js
@@ -2,7 +2,6 @@ const onInstanceCreate = require("./definitions/onInstanceCreate")
 const onMessageUpdate = require("./definitions/onMessageUpdate")
 const webhookHandler = require("./definitions/webhookHandler")
 const onVoteRequestUpdate = require("./definitions/onVoteRequestUpdate")
-const analyticsUpdateSheet = require("./definitions/auxillary/analyticsUpdateSheet")
 const batchJobs = require("./definitions/batchJobs")
 const healthcheck = require("./definitions/healthcheck")
 
@@ -11,6 +10,5 @@ exports.onInstanceCreate = onInstanceCreate.onInstanceCreate
 exports.onMessageUpdate = onMessageUpdate.onMessageUpdate
 exports.webhookHandler = webhookHandler.webhookHandler
 exports.onVoteRequestUpdate = onVoteRequestUpdate.onVoteRequestUpdate
-exports.analyticsUpdateSheet = analyticsUpdateSheet.analyticsUpdateSheet
 exports.batchJobs = batchJobs
 exports.healthcheck = healthcheck.healthcheck
