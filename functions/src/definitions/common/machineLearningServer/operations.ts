3aaf46444029e094ee7606d308019c2339cc744d
stonefruit <jeromeng888@gmail.com>
2023-05-28 21:59:12 +0800

    update operations.ts

--- functions/src/definitions/common/machineLearningServer/operations.ts
+++ functions/src/definitions/common/machineLearningServer/operations.ts
@@ -1,53 +1,53 @@
-//operations.ts
-
-import { defineString } from "firebase-functions/params";
-import axios, { AxiosResponse } from "axios";
-import * as functions from 'firebase-functions';
+import { defineString } from "firebase-functions/params"
+import axios, { AxiosError } from "axios"
+import * as functions from "firebase-functions"
 
 const embedderHost = defineString("EMBEDDER_HOST")
 
 interface EmbedResponse {
-  data: {
-    embedding: number[];
-  };
+  embedding: number[]
 }
 
 interface TrivialResponse {
-  data: {
-    prediction: string;
-  };
+  prediction: string
 }
 
 async function getEmbedding(text: string): Promise<number[]> {
   const data = {
-    text: text
+    text: text,
   }
-  const response = await callAPI<EmbedResponse>("embed", data);
+  const response = await callAPI<EmbedResponse>("embed", data)
   return response.data.embedding
 }
 
 async function checkTrivial(text: string): Promise<string> {
   const data = {
-    text: text
+    text: text,
   }
-  const response = await callAPI<TrivialResponse>("checkTrivial", data);
+  const response = await callAPI<TrivialResponse>("checkTrivial", data)
   return response.data.prediction
 }
 
-async function callAPI<T>(endpoint: string, data: object): Promise<AxiosResponse> {
-  const response: AxiosResponse<T> = await axios({
-    method: "POST", // Required, HTTP method, a string, e.g. POST, GET
-    url: `${embedderHost.value()}/${endpoint}`,
-    data: data,
-    headers: {
-      "Content-Type": "application/json",
-      "apikey": process.env.ML_SERVER_TOKEN ?? "",
-    },
-  }).catch((error) => {
-    functions.logger.log(error.response.data);
-    throw new Error("An error occurred calling the machine learning API");
-  });
-  return response
+async function callAPI<T>(endpoint: string, data: object) {
+  try {
+    const response = await axios<T>({
+      method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+      url: `${embedderHost.value()}/${endpoint}`,
+      data: data,
+      headers: {
+        "Content-Type": "application/json",
+        apikey: process.env.ML_SERVER_TOKEN ?? "",
+      },
+    })
+    return response
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      functions.logger.log(error.message)
+    } else {
+      functions.logger.log(error)
+    }
+    throw new Error("An error occurred calling the machine learning API")
+  }
 }
 
 export { getEmbedding, checkTrivial }
