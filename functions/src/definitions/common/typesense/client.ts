fe801166871e55a5b0b7c5fcb83b16aa5aaf725e
stonefruit <jeromeng888@gmail.com>
2023-05-28 23:44:44 +0800

    Get integration tests to run partially

--- functions/src/definitions/common/typesense/client.ts
+++ functions/src/definitions/common/typesense/client.ts
@@ -1,20 +1,22 @@
-// typesenseClient.ts
-import Typesense from "typesense";
-import { defineString } from "firebase-functions/params";
-
+import Typesense from "typesense"
+import { defineString } from "firebase-functions/params"
 
 const typesenseHost = defineString("TYPESENSE_HOST")
+const typesensePort = defineString("TYPESENSE_PORT")
+const typesenseProtocol = defineString("TYPESENSE_PROTOCOL")
 
 function getClient() {
   return new Typesense.Client({
-    "nodes": [{
-      "host": typesenseHost.value(),
-      "port": 443,
-      "protocol": "https",
-    }],
-    "apiKey": process.env.TYPESENSE_TOKEN ?? "",
-    "connectionTimeoutSeconds": 10
-  });
+    nodes: [
+      {
+        host: typesenseHost.value(),
+        port: Number(typesensePort.value()),
+        protocol: typesenseProtocol.value(),
+      },
+    ],
+    apiKey: process.env.TYPESENSE_TOKEN ?? "",
+    connectionTimeoutSeconds: 10,
+  })
 }
 
 export { getClient }
