9cd18e42651e7d09f25f2e1306c1d531fab56270
stonefruit <jeromeng888@gmail.com>
2023-06-30 11:03:32 +0800

    update esModuleInterop

--- functions/src/definitions/webhookHandler.ts
+++ functions/src/definitions/webhookHandler.ts
@@ -1,6 +1,6 @@
 import * as functions from "firebase-functions"
 import * as admin from "firebase-admin"
-import * as express from "express"
+import express from "express"
 
 import { userHandlerWhatsapp } from "./userHandlers"
 import { checkerHandlerWhatsapp } from "./checkerHandlers"
