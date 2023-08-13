199e1e2e6d763f9df751f57fb2422db67849b1fc
stonefruit <jeromeng888@gmail.com>
2023-08-13 19:38:28 +0800

    update prettier

--- integration-tests/update-env.ts
+++ integration-tests/update-env.ts
@@ -1,62 +1,62 @@
-import _ from 'lodash';
-import fs from 'fs';
-import env from './env.json';
-import * as constants from '../functions/src/definitions/common/constants';
+import _ from "lodash"
+import fs from "fs"
+import env from "./env.json"
+import * as constants from "../functions/src/definitions/common/constants"
 
 const factCheckerKeys = Object.keys(constants.FACTCHECKER_BOT_RESPONSES).map(
   (key) => {
-    return `__CONSTANTS__.FACTCHECKER_BOT_RESPONSES.${key}`;
+    return `__CONSTANTS__.FACTCHECKER_BOT_RESPONSES.${key}`
   }
-);
+)
 const userKeys = Object.keys(constants.USER_BOT_RESPONSES).map((key) => {
-  return `__CONSTANTS__.USER_BOT_RESPONSES.${key}`;
-});
+  return `__CONSTANTS__.USER_BOT_RESPONSES.${key}`
+})
 
 //combine the two lists
-const allKeys = factCheckerKeys.concat(userKeys);
+const allKeys = factCheckerKeys.concat(userKeys)
 
 //find if each key in allKeys is inside env.values, if not, add it
 allKeys.forEach((key) => {
   const found = env.values.find((value) => {
-    return value.key === key;
-  });
+    return value.key === key
+  })
   if (!found) {
     env.values.push({
       key,
-      value: '',
+      value: "",
       enabled: true,
-    });
+    })
   }
-});
+})
 
 //loop through each key in env.values, if it starts with "__CONSTANTS___" but is not in allKeys, remove it
 const envWithRemovedValues = env.values.filter((value) => {
-  if (value.key.startsWith('__CONSTANTS__.')) {
-    const key = value.key;
+  if (value.key.startsWith("__CONSTANTS__.")) {
+    const key = value.key
     const found = allKeys.find((allKey) => {
-      return allKey === key;
-    });
+      return allKey === key
+    })
     if (!found) {
-      return false;
+      return false
     } else {
-      return true;
+      return true
     }
   } else {
-    return true;
+    return true
   }
-});
+})
 
 const envWithReplacedValues = envWithRemovedValues.map((value) => {
-  if (value.key.startsWith('__CONSTANTS__.')) {
-    const key = value.key.replace('__CONSTANTS__.', '');
-    const newValue = _.get(constants, key);
-    value.value = newValue;
-    return value;
+  if (value.key.startsWith("__CONSTANTS__.")) {
+    const key = value.key.replace("__CONSTANTS__.", "")
+    const newValue = _.get(constants, key)
+    value.value = newValue
+    return value
   } else {
-    return value;
+    return value
   }
-});
+})
 
-env.values = envWithReplacedValues;
+env.values = envWithReplacedValues
 
-fs.writeFileSync('env.json', JSON.stringify(env, null, 2));
+fs.writeFileSync("env.json", JSON.stringify(env, null, 2))
