e555114374a03ca370afaf7e1dd49b1835299c86
TY0018 <111295767+TY0018@users.noreply.github.com>
2023-11-13 15:15:28 +0800

    feat: dashboard charts

---
+++ checkers-app/src/config/charts-config.tsx
@@ -0,0 +1,78 @@
+export const chartsConfig = {
+  chart: {
+    width: "100%",
+    // height: auto,
+    toolbar: {
+      show: false,
+    },
+  },
+  title: {
+    show: "",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  xaxis: {
+    axisTicks: {
+      show: false,
+    },
+    axisBorder: {
+      show: false,
+    },
+    labels: {
+      show:false,
+      style: {
+        colors: "#fff",
+        fontSize: "13px",
+        fontFamily: "inherit",
+        fontWeight: 300,
+      },
+    },
+  },
+  yaxis: {
+    labels: {
+      style: {
+        colors: "#fff",
+        fontSize: "13px",
+        fontFamily: "inherit",
+        fontWeight: 300,
+      },
+    },
+  },
+  grid: {
+    show: true,
+    borderColor: "#ffffff40",
+    strokeDashArray: 5,
+    xaxis: {
+      lines: {
+        show: true,
+      },
+    },
+    padding: {
+      top: 5,
+      right: 20,
+    },
+  },
+  colors: ["#00a8b1"],
+  fill: {
+    opacity: 0.8,
+  },
+  stroke: {
+    curve: 'smooth',
+  },
+  animations: {
+    enabled: true,
+    easing: 'easeinout',
+    speed: 800,
+    animateGradually: {
+      enabled: true,
+      delay: 150
+    },
+    dynamicAnimation: {
+      enabled: true,
+      speed: 350
+    }
+  }
+};
+
+export default chartsConfig;
