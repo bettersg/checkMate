e757a404d57ab53b70aef5e4f62e01b4c2b91d3d
Brennan <tanjialebrennan@gmail.com>
2024-02-12 15:36:37 +0800

    feat: add tanstack library

--- checkers-app/src/main.tsx
+++ checkers-app/src/main.tsx
@@ -2,12 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 2 }
+  }
+})
 
-
+const isDev = !!import.meta.env.DEV
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {isDev && <ReactQueryDevtools />}
+    </QueryClientProvider>
   </React.StrictMode>,
 )
