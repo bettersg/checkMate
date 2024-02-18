ebe2cb305607a0e9b214504ba062f777cd8ae05c
Brennan <tanjialebrennan@gmail.com>
2024-02-19 01:26:10 +0800

    feat: add name update to mutation hook

--- checkers-app/src/pages/Onboarding.tsx
+++ checkers-app/src/pages/Onboarding.tsx
@@ -15,31 +15,63 @@ const NameForm = ({ name, setName }: { name: string, setName: React.Dispatch<Rea
   )
 }
 
-
-interface StepInstructions {
-  'step': number;
-  'text': string;
+const ThankYou = ({ name }: { name: string }) => {
+  return (
+    <div>
+      <p>Thank you and welcome, {name}!</p>
+    </div>
+  );
 }
 
-const stepInstructions: StepInstructions[] = [
-  {
-    'step': 1,
-    'text': `Welcome to our community of CheckMates! 👋🏻 We're grateful to have you on board to combat misinformation and scams. 🙇‍♀️🙇🏻 We'd love to get to know you better - could you *reply to this message* and share your name with us? (Reply to this message by swiping right)!`,
-    // 'action': <NameForm  />
-  },
-  {
-    'step': 2,
-    'text': `Thank you and welcome, Brennan! We're thrilled to have you on board as we work together to combat misinformation and scams.😊 By using the CheckMate bot, you are accepting our privacy policy which can be found here: https://bit.ly/checkmate-privacy`
-  },
-  {
-    'step': 3,
-    'text': `To ensure you're equipped with the necessary skills to identify misinformation and scams, let's start with a quick quiz. 📝 Simply follow the link (https://bit.ly/checkmates-quiz) to take the quiz. Once you've completed it, come back to this chat and click on "I've done the quiz!" to notify me. Let's get started! 🤖`
-  },
-  {
-    'step': 4,
-    'text': `Awesome! Now that you know how to identify misinformation and scams, you are ready to help us combat them! Use the '/info' command for more resources. Thanks again for joining our community of CheckMates!`
-  }
-]
+const StepOne = () => {
+  return (
+    <div>
+      <p>Welcome to our community of CheckMates! 👋🏻 We're grateful to have you on board to combat misinformation and scams. 🙇‍♀️🙇🏻 We'd love to get to know you better - could you share your name with us?</p>
+    </div>
+  )
+
+}
+
+const StepTwo = () => {
+
+  return (
+    <div>
+      <p>We're thrilled to have you on board as we work together to combat misinformation and scams.😊 By using the CheckMate bot, you are accepting our privacy policy which can be found here:</p>
+      <br />
+      <a className="underline text-checkLink" href="https://bit.ly/checkmate-privacy">
+        https://bit.ly/checkmate-privacy
+      </a>
+    </div>
+  )
+
+}
+
+const StepThree = () => {
+  return (
+    <p>
+      To ensure you're equipped with the necessary skills to identify misinformation and scams, let's start with a quick quiz. 📝 Simply follow the link (<a className="underline text-checkLink" href="https://bit.ly/checkmates-quiz)">https://bit.ly/checkmates-quiz)</a> to take the quiz. Once you've completed it, come back to this chat and click on "I've done the quiz!" to notify me.Let's get started! 🤖
+    </p>
+  )
+}
+
+const StepFour = () => {
+  return (
+    <p>
+      Awesome! Now that you know how to identify misinformation and scams, you are ready to help us combat them! Thanks again for joining our community of CheckMates!
+    </p>
+  )
+}
+
+
+const steps: { [key: number]: JSX.Element } = {
+  1: <StepOne />,
+  2: <StepTwo />,
+  3: <StepThree />,
+  4: <StepFour />
+}
+
+const numberOfSteps = Object.keys(steps).length;
+
 
 const Onboarding = ({ factChecker }: { factChecker: FactChecker }) => {
   const [name, setName] = useState("");
@@ -47,28 +79,19 @@ const Onboarding = ({ factChecker }: { factChecker: FactChecker }) => {
 
   const updateFactChecker = useUpdateFactChecker();
   const handleOnCompleteOnboarding = () => {
-    updateFactChecker.mutate({ ...factChecker, isOnboardingComplete: true })
+    updateFactChecker.mutate({ ...factChecker, name, isOnboardingComplete: true })
   }
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <p className="max-w-[250px] break-all">
-        {/* {JSON.stringify(factChecker)} */}
-        User set name: {name}
-      </p>
-      <h1 className="text-checkPrimary600 font-medium text-[20px]">ONBOARDING PAGE</h1>
+    <div className="bg-checkBG min-h-screen flex flex-col items-center justify-center border border-black mt-4">
+      <h1 className="text-checkPrimary600 font-medium text-[20px]">FactChecker's Onboarding</h1>
       <div className="p-5">
-        {stepInstructions.map((step) => {
-          return (
-            step.step === currentStep && (
-              <div>
-                <p className="font-bold">Step: {step.step} out of {stepInstructions.length}</p>
-                {step.step === 1 && <NameForm name={name} setName={setName} />}
-                <p className="pb-3">{step.text}</p>
-              </div>)
-          )
-        })}
-        {currentStep !== stepInstructions.length ?
+        <p className="font-bold">Step: {currentStep} out of {numberOfSteps}</p>
+        {currentStep === 2 && <ThankYou name={name} />}
+        <p className="pb-3">{steps[currentStep]}</p>
+        {currentStep === 1 && <NameForm name={name} setName={setName} />}
+
+        {currentStep !== numberOfSteps ?
           <button className="p-2 font-medium rounded-[50px] bg-checkPrimary600 border" onClick={() => setCurrentStep(currentStep + 1)}>Next step</button> :
           <button className="p-2 font-medium rounded-[50px] bg-checkPrimary600 border" onClick={handleOnCompleteOnboarding}>Complete Onboarding</button>
         }
