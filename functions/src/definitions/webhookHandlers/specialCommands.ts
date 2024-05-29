f84574e351bee9fbaabe303b337c096e05530cf5
Bing Wen Tan <bingwen.cpf@gmail.com>
2024-05-29 22:05:22 +0800

    switched back specialCommands to whatsapp

--- functions/src/definitions/webhookHandlers/specialCommands.ts
+++ functions/src/definitions/webhookHandlers/specialCommands.ts
@@ -102,7 +102,7 @@ const mockDb = async function () {
     numCorrectVotes: 0,
     numNonUnsureVotes: 0,
     numVerifiedLinks: 0,
-    preferredPlatform: "telegram",
+    preferredPlatform: "whatsapp",
     lastVotedTimestamp: null,
     getNameMessageId: null,
     leaderboardStats: {
