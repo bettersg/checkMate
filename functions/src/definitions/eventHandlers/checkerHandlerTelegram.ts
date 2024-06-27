61cd00862aa58d5b1116266f7f71a63b56d2ea19
joshuagohez <jgoh.ezhong@gmail.com>
2024-06-27 17:14:29 +0800

    onboarding tele bot skeleton workflow w/o API integration

--- functions/src/definitions/eventHandlers/checkerHandlerTelegram.ts
+++ functions/src/definitions/eventHandlers/checkerHandlerTelegram.ts
@@ -38,17 +38,239 @@ bot.onText(/\/start/, async (msg) => {
       .get()
 
     //check if user exists in database
-    if (userQuerySnap.size > 0) {
+    if (userQuerySnap?.data()?.isOnboardingComplete) {
       await bot.sendMessage(
         chatId,
         `Welcome to the checker bot! Press the CheckMate's Portal button to access our dashboard. You'll also get notified when there are new messages to check.`
       )
       //add function to start receiving messsages
     } else {
-      await bot.sendMessage(
-        chatId,
-        `Welcome to the checker bot! Press the CheckMate's Portal button to onboard and access our dashboard. Once onboarded, you'll get notified when there are new messages to check.`
-      )
+      // await bot.sendMessage(
+      //   chatId,
+      //   `Welcome to the checker bot! Press the CheckMate's Portal button to onboard and access our dashboard. Once onboarded, you'll get notified when there are new messages to check.`
+      // )
+      bot.onText(/\/onboard/, async (msg) => {
+        const chatId = msg.chat.id
+        const namePrompt = await bot.sendMessage(
+          msg.chat.id,
+          "Hi, what's your full name?",
+          {
+            reply_markup: {
+              force_reply: true,
+            },
+          }
+        )
+        bot.onReplyToMessage(chatId, namePrompt.message_id, async (nameMsg) => {
+          const name = nameMsg.text
+          const telegramId = nameMsg.from.id
+
+          const numberPrompt = await bot.sendMessage(
+            chatId,
+            `Hello ${name}! What is your HP no. in +(country code) (HP no.) format e.g +65 12341234`,
+            {
+              reply_markup: {
+                force_reply: true,
+              },
+            }
+          )
+          bot.onReplyToMessage(
+            chatId,
+            numberPrompt.message_id,
+            async (numberMsg) => {
+              const whatsappId = numberMsg.text
+
+              //create checker entry in DB
+              await bot.sendMessage(chatId, "Create checker")
+              //   const checkerQuerySnap = await db
+              //     .collection("checkers")
+              //     .where("telegramId", "==", telegramId)
+              //     .get();
+              //   if (checkerQuerySnap.size > 0) {
+              //     const checkerSnap = checkerQuerySnap.docs[0];
+              //     console.log(checkerSnap);
+              //   } else if (checkerQuerySnap.size === 0) {
+              //     postCheckerHandler({ name, type: "human", telegramId });
+              //   } else {
+              //     logger.error(
+              //       `Multiple checkers with TelegramID ${telegramId} found`
+              //     );
+              //   }
+
+              while (true) {
+                await bot.sendMessage(chatId, "Sent OTP")
+                // postOTPHandler({ checkerId: telegramId, whatsappId });
+
+                await bot.sendMessage(
+                  chatId,
+                  `We have sent a 6-digit OTP to ${whatsappId}. Please check your Whatsapp for the OTP.`
+                )
+                const otpPrompt = await bot.sendMessage(
+                  msg.chat.id,
+                  `Verify your OTP:`,
+                  {
+                    reply_markup: {
+                      force_reply: true,
+                    },
+                  }
+                )
+                bot.onReplyToMessage(
+                  chatId,
+                  otpPrompt.message_id,
+                  async (otpMsg) => {
+                    const otp = otpMsg.text
+
+                    // call postOTP to check matching OTP
+                    await bot.sendMessage(chatId, "Verified OTP")
+
+                    //   res = await checkOTPHandler({ checkerId: telegramId, otp });
+
+                    //   if (
+                    //     res.message == "OTP verified successfully" ||
+                    //     res.message == "Existing checker found"
+                    //   ) {
+
+                    await bot.sendMessage(
+                      chatId,
+                      `Thank you for verifying your Whatsapp number. Please proceed to complete the onboarding quiz: https://better-sg.typeform.com/to/MlihTUDx`,
+                      {
+                        reply_markup: JSON.stringify({
+                          inline_keyboard: [
+                            [
+                              {
+                                text: "Yes, I have finished the onboarding quiz",
+                                callback_data: "1",
+                              },
+                            ],
+                            [
+                              {
+                                text: "No, I will get to it",
+                                callback_data: "2",
+                              },
+                            ],
+                          ],
+                        }),
+                      }
+                    )
+
+                    bot.on(
+                      "callback_query",
+                      async function onCallbackQuery(callbackQuery) {
+                        const action = callbackQuery.data
+                        const msg = callbackQuery.message
+                        let text
+
+                        if (action === "1") {
+                          // check quiz completion
+                          // const checkerQuerySnap = await db
+                          //   .collection("checkers")
+                          //   .where("telegramId", "==", telegramId)
+                          //   .get();
+
+                          // if (checkerSnap.data().isOnboardingComplete) {
+                          //might need to open new field for typeform completion
+                          text =
+                            "Thank you for completing the quiz. Please add the CheckMate Whatsapp bot: https://wa.me/6580432188."
+                          bot.editMessageText(text, {
+                            chat_id: chatId,
+                            message_id: msg.message_id,
+                            reply_markup: JSON.stringify({
+                              inline_keyboard: [
+                                [
+                                  {
+                                    text: "Yes, I have added the WA bot",
+                                    callback_data: "1",
+                                  },
+                                ],
+                                [
+                                  {
+                                    text: "No, I will get to it",
+                                    callback_data: "2",
+                                  },
+                                ],
+                              ],
+                            }),
+                          })
+
+                          bot.on(
+                            "callback_query",
+                            async function onCallbackQuery(callbackQuery) {
+                              const action = callbackQuery.data
+                              const msg = callbackQuery.message
+                              let text
+
+                              if (action === "1") {
+                                // check WA bot completion
+
+                                text =
+                                  "Thank you for adding the WA bot. Please add the CheckMate Checker's telegram bot: https://t.me/CheckMate_Checker_Bot."
+                                bot.editMessageText(text, {
+                                  chat_id: chatId,
+                                  message_id: msg.message_id,
+                                  reply_markup: JSON.stringify({
+                                    inline_keyboard: [
+                                      [
+                                        {
+                                          text: "Yes, I have added the telegram bot",
+                                          callback_data: "1",
+                                        },
+                                      ],
+                                      [
+                                        {
+                                          text: "No, I will get to it",
+                                          callback_data: "2",
+                                        },
+                                      ],
+                                    ],
+                                  }),
+                                })
+
+                                bot.on(
+                                  "callback_query",
+                                  async function onCallbackQuery(
+                                    callbackQuery
+                                  ) {
+                                    const action = callbackQuery.data
+                                    const msg = callbackQuery.message
+                                    let text
+
+                                    if (action === "1") {
+                                      // check tele bot completion
+
+                                      text =
+                                        "Thank you for adding the telegram bot. Please refer to CheckMate wiki for more information: https://checkmate.sg/."
+                                      await bot.editMessageText(text, {
+                                        chat_id: chatId,
+                                        message_id: msg.message_id,
+                                      })
+
+                                      // await checkerSnap.ref.update({ isOnboardingComplete: true })
+
+                                      await bot.sendMessage(
+                                        chatId,
+                                        `You have successfully onboarded as a CheckMate Checker!`
+                                      )
+                                    }
+                                  }
+                                )
+                              }
+                            }
+                          )
+                          // }
+                        }
+                      }
+                    )
+
+                    //   }
+
+                    // cycle back to otpPrompt above
+                  }
+                )
+                return
+              }
+            }
+          )
+        })
+      })
     }
   } else {
     logger.log("No user id found")
