cef3481abba464456de256b77a09d92ea39da41f
Bing Wen Tan <bingwen.cpf@gmail.com>
2024-09-08 21:32:16 +0800

    updated cloud task queue name

--- functions/src/definitions/common/cloudTasks.ts
+++ functions/src/definitions/common/cloudTasks.ts
@@ -5,18 +5,22 @@ import { GoogleAuth } from "google-auth-library"
 async function enqueueTask(
   payload: Record<string, any>,
   functionName: string,
-  delaySeconds: number = 0
+  delaySeconds: number = 0,
+  location: string = "asia-southeast1"
 ) {
   try {
     const targetUri = await getFunctionUrl(functionName)
-    const queue = getFunctions().taskQueue(functionName)
+    const queue = getFunctions().taskQueue(
+      `locations/${location}/functions/${functionName}`
+    )
     await queue.enqueue(payload, {
       scheduleDelaySeconds: delaySeconds,
       uri: targetUri,
     })
   } catch (e) {
     if (process.env.ENVIRONMENT === "DEV") {
-      logger.log("Unable to enqueue task")
+      // cant seem to test this in local emulator as I get an error Failed to determine service account. Initialize the SDK with service account credentials or set service account ID as an app option.
+      logger.log("Unable to enqueue task..")
     } else {
       logger.error(`Error enqueuing task for function ${functionName}: ${e}`)
     }
