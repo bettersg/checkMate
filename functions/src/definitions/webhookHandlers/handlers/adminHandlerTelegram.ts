055b54229ea9a0625d4b80e09fdf7a70357e1dc5
Bing Wen Tan <bingwen.cpf@gmail.com>
2024-10-12 09:48:58 +0800

    improved wordings

--- functions/src/definitions/webhookHandlers/handlers/adminHandlerTelegram.ts
+++ functions/src/definitions/webhookHandlers/handlers/adminHandlerTelegram.ts
@@ -35,20 +35,7 @@ adminBot.on(message("new_chat_members"), async (ctx) => {
       const username = member.username ? ` @${member.username}` : ""
       const message = `Hi ${name}${username},
     
-Welcome to the CheckMate comms channel 👋
-
-This channel will be used to:
-1. Inform Checkers of updates/improvements
-
-2. Inform Checkers of any downtime
-
-3. Share relevant links from reputable sources to aid fact checking. Note: Beyond this, Checkers should not discuss the voting, as this may make the collective outcome biased.
-
-4. You can refer to <a href="https://bit.ly/checkmates-wiki">our fact-checking wiki</a> for more tips on safe checking 😊
-
-5. To explore more resources, head to <a href="https://t.me/CheckMate_Checker_Bot">your personal Checker's bot</a> and type / resources
-
-If you've any feedback or queries, you can share them in the chat too 🤗`
+Thanks for joining CheckMate as a checker 🙏🏻 and welcome to the new checkers onboarding group 🎉! Do check out the pinned message above to get oriented.`
 
       return ctx.reply(message, {
         link_preview_options: {
